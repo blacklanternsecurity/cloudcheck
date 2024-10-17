eb67f254dc9ee91f87ccda7767429034de99ebfc
TheTechromancer <TheTechromancer@protonmail.com>
2024-10-17 01:13:56 -0400

    bump versions

--- cloudcheck/providers/base.py
+++ cloudcheck/providers/base.py
@@ -4,7 +4,7 @@ import ipaddress
 import traceback
 import regex as re
 from datetime import datetime
-from radixtarget import RadixTarget
+from radixtarget import Target
 from typing import Dict, List, Union
 from pydantic import BaseModel, field_validator
 
@@ -17,7 +17,21 @@ asndb = None
 
 
 base_headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "priority": "u=0, i",
+    "referer": "https://www.google.com/",
+    "sec-ch-ua": '"Chromium";v="127", "Not)A;Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Linux"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "cross-site",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
 }
 
 
@@ -49,7 +63,7 @@ class BaseCloudProvider:
     def __init__(self, j):
         self._log = None
         self.ranges = set()
-        self.radix = RadixTarget()
+        self.radix = Target()
         if j is not None:
             p = CloudProviderJSON(**j)
             self.update_domains(
@@ -142,10 +156,9 @@ class BaseCloudProvider:
     def parse_response(self, response):
         pass
 
-    @classmethod
     @property
-    def name(cls):
-        return cls.__name__
+    def name(self):
+        return self.__class__.__name__
 
     @property
     def log(self):
